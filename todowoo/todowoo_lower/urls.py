006afbd1071bbd6d54c6a6281df2e66d5d4e5900
mukund2124 <mukundkushwaha2124@gmail.com>
2020-08-25 13:09:51 +0545

    commiting rest

---
+++ todowoo/todowoo_lower/urls.py
@@ -0,0 +1,43 @@
+"""todowoo_lower URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from todo import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+
+    path('admin/', admin.site.urls),
+
+    path('logout/', views.logoutuser, name='logoutuser'),
+
+    path('signup/', views.signupuser, name='signupuser'),
+
+    path('login/', views.loginuser, name='loginuser'),
+
+    path('create/', views.createtodo, name='createtodo'),
+
+    path('current/', views.currentalogin, name='currentalogin'),
+
+    path('completed/', views.completedtodo, name='completedtodo'),
+
+ # user can click on todo and view full info associated with it
+    path('todo/<int:todo_pk>', views.viewstodo, name='viewstodo'),
+
+    path('todo/<int:todo_pk>/complete/', views.completetodo, name='completetodo'),
+
+    path('todo/<int:todo_pk>/delete/', views.deletetodo, name='deletetodo'),
+]
